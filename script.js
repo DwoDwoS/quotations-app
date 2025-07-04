841a8d775bd4b1c6c8a07982e5241eae52b85afc
Elouan <elouangauriaud@gmail.com>
2025-07-04 12:08:05 +0200

    Etape 5: Ajout de citations à la suite

--- script.js
+++ script.js
@@ -1,11 +1,31 @@
-const form = document.getElementById('quote-form');
+const FORM = document.getElementById("quote-form");
 
-form.addEventListener('submit', function(event) {
+FORM.addEventListener("submit", function(event) {
   event.preventDefault();
 
-  const text = document.getElementById('text').value;
-  const author = document.getElementById('authorName').value;
+  const TEXT = document.getElementById("text").value;
+  const AUTHOR = document.getElementById("author").value;
 
-  console.log('Citation :', text);
-  console.log('Auteur/Autrice :', author);
+  console.log("Citation :", TEXT);
+  console.log("Auteur/Autrice :", AUTHOR);
+
+  addQuote(TEXT, AUTHOR);
 });
+
+function addQuote(quote, author) {
+  const QUOTETEXT = document.createElement("p");
+  QUOTETEXT.className = "text";
+  QUOTETEXT.textContent = quote;
+
+  const QUOTEAUTHOR = document.createElement("p");
+  QUOTEAUTHOR.className = "author";
+  QUOTEAUTHOR.textContent = author;
+
+  const QUOTEDIV = document.createElement("div");
+  QUOTEDIV.className = "quote";
+  QUOTEDIV.appendChild(QUOTETEXT);
+  QUOTEDIV.appendChild(QUOTEAUTHOR);
+
+  const QUOTELIST = document.getElementById("quote-list");
+  QUOTELIST.appendChild(QUOTEDIV);
+}
